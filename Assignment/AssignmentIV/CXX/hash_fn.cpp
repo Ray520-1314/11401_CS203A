ab6444189fcbd95d084750ac6c2b56d9592b75dd
Ray520-1314 <a40308002@gmail.com>
2026-01-10 22:37:07 +0800

    Update hash_fn.cpp

--- Assignment/AssignmentIV/CXX/hash_fn.cpp
+++ Assignment/AssignmentIV/CXX/hash_fn.cpp
@@ -7,20 +7,24 @@
     This file contains the hash functions for integer and string keys.
 
    Development History:
-    - 2025/11/11: Initial implementation
-    - 2025/11/17: Refactored to use hash_fn.hpp
+    - 2026/01/10 gpt help me
 
    Developer: Yu-Feng Huang <yfhuang@saturn.yzu.edu.tw>
  */
 #include "hash_fn.hpp"
 
 int myHashInt(int key, int m) {
-    // TODO: replace with your own design
-    return key % m;  // basic division method
+    unsigned int x = static_cast<unsigned int>(key);
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = ((x >> 16) ^ x) * 0x45d9f3b;
+    x = (x >> 16) ^ x;
+    return static_cast<int>(x % m);
 }
 
 int myHashString(const std::string& str, int m) {
-    unsigned long hash = 0;
-    // TODO: replace with your own design
-    return static_cast<int>(hash % m);  // basic division method
+    unsigned long hash = 5381;
+    for (unsigned char c : str) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return static_cast<int>(hash % m);
 }
